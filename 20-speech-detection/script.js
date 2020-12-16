29aeb0edad0ab75a0c9500d4a91fed1a366b101f
asaran97 <asaran234@gmail.com>
2020-12-17 00:28:04 +0530

    added speech recognition

---
+++ 20-speech-detection/script.js
@@ -0,0 +1,27 @@
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", e => {
+  const transcript = Array.from(e.results)
+    .map(res => res[0])
+    .map(res => res.transcript)
+    .join("");
+
+  p.textContent = transcript;
+
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+    words.appendChild(p);
+  }
+  // console.log(transcript);
+});
+
+recognition.addEventListener("end", recognition.start);
+recognition.start();
